674c33a4d35471122265d0303a2c9334f8b65f70
Sixshaman <shaman1234@gmail.com>
2020-12-30 13:29:51 +0300

    More function prototype fixes

--- DDSVulkanFunctionsInclude.h
+++ DDSVulkanFunctionsInclude.h
@@ -4,10 +4,13 @@
 
 #include <vulkan/vulkan_core.h>
 
-extern PFN_vkCreateImage vkCreateImage;
+extern "C"
+{
+	extern PFN_vkCreateImage vkCreateImage;
 
-#ifdef VK_EXT_debug_utils
-	extern PFN_vkSetDebugUtilsObjectNameEXT vkSetDebugUtilsObjectNameEXT;
-#endif
+	#ifdef VK_EXT_debug_utils
+		extern PFN_vkSetDebugUtilsObjectNameEXT vkSetDebugUtilsObjectNameEXT;
+	#endif
+}
 
 #endif // VK_NO_PROTOTYPES
